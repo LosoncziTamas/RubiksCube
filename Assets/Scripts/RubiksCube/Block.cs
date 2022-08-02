e6b8badf59c146a3b075c2fa63e93505186e0b1b
Tamás Losonczi <losonczi.tamas91@gmail.com>
2022-08-02 21:40:01 +0200

    work on adjacent retrieval

--- Assets/Scripts/RubiksCube/Block.cs
+++ Assets/Scripts/RubiksCube/Block.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,27 @@ namespace RubiksCube
     public class Block : MonoBehaviour
     {
         [SerializeField] private List<BlockSide> _sides;
-        
-        
+
+        private Collider[] _colliders;
+
+        private void GetHorizontalPieces()
+        {
+            var extents = new Vector3(3, 0.25f, 3);
+            // TODO use non-alloc
+            _colliders = Physics.OverlapBox(transform.position, extents);
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Space(100);
+            if (GUILayout.Button("Get Horizontal Pieces"))
+            {
+                GetHorizontalPieces();
+                foreach (var c in _colliders)
+                {
+                    Debug.Log(c.transform.position);
+                }
+            }
+        }
     }
 }
